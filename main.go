be037bf01f139aa0a7778b039c442282bf040e21
1legality <markis.landry@gmail.com>
2017-10-17 19:59:57 -0400

    factorise functions

--- main.go
+++ main.go
@@ -52,59 +52,65 @@ func file2lines(filePath string) []string {
 
 // TODO : findNextMove, undoLastMove, RecursiveFunction "resolve" which returns true or false
 
-func findNextMove(line int, column int) (bool, string) {
+func findNextMove() (bool, move) {
 	// Use Konami order up down left right
-	if line >= 2 &&
-		puzzleState[line-1][column] == 1 &&
-		puzzleState[line-2][column] == 2 &&
-		!isMoveBanned(move{line, column, "↑", puzzleState}) {
-		// up
-		moves = append(moves, move{line, column, "↑", puzzleState})
+	var initialPuzzleState = puzzleState
+	for line := 0; line < 7; line++ {
+		for column := 0; column < 7; column++ {
+			if puzzleState[line][column] == 1 {
+				if line >= 2 &&
+					puzzleState[line-1][column] == 1 &&
+					puzzleState[line-2][column] == 2 &&
+					!isMoveBanned(move{line, column, "↑", puzzleState}) {
+					// up
+					puzzleState[line][column] = 2
+					puzzleState[line-1][column] = 2
+					puzzleState[line-2][column] = 1
 
-		puzzleState[line][column] = 2
-		puzzleState[line-1][column] = 2
-		puzzleState[line-2][column] = 1
+					return true, move{line, column, "↑", initialPuzzleState}
+				} else if line <= 4 &&
+					puzzleState[line+1][column] == 1 &&
+					puzzleState[line+2][column] == 2 &&
+					!isMoveBanned(move{line, column, "↓", puzzleState}) {
+					// down
+					puzzleState[line][column] = 2
+					puzzleState[line+1][column] = 2
+					puzzleState[line+2][column] = 1
 
-		return true, "↑"
-	} else if line <= 4 &&
-		puzzleState[line+1][column] == 1 &&
-		puzzleState[line+2][column] == 2 &&
-		!isMoveBanned(move{line, column, "↓", puzzleState}) {
-		// down
+					return true, move{line, column, "↓", initialPuzzleState}
+				} else if column >= 2 &&
+					puzzleState[line][column-1] == 1 &&
+					puzzleState[line][column-2] == 2 &&
+					!isMoveBanned(move{line, column, "←", puzzleState}) {
+					// left
+					puzzleState[line][column] = 2
+					puzzleState[line][column-1] = 2
+					puzzleState[line][column-2] = 1
 
-		moves = append(moves, move{line, column, "↓", puzzleState})
+					return true, move{line, column, "←", initialPuzzleState}
+				} else if column <= 4 &&
+					puzzleState[line][column+1] == 1 &&
+					puzzleState[line][column+2] == 2 &&
+					!isMoveBanned(move{line, column, "→", puzzleState}) {
+					// right
+					puzzleState[line][column] = 2
+					puzzleState[line][column+1] = 2
+					puzzleState[line][column+2] = 1
 
-		puzzleState[line][column] = 2
-		puzzleState[line+1][column] = 2
-		puzzleState[line+2][column] = 1
-
-		return true, "↓"
-	} else if column >= 2 &&
-		puzzleState[line][column-1] == 1 &&
-		puzzleState[line][column-2] == 2 &&
-		!isMoveBanned(move{line, column, "←", puzzleState}) {
-		// left
-
-		moves = append(moves, move{line, column, "←", puzzleState})
-		puzzleState[line][column] = 2
-		puzzleState[line][column-1] = 2
-		puzzleState[line][column-2] = 1
-
-		return true, "←"
-	} else if column <= 4 &&
-		puzzleState[line][column+1] == 1 &&
-		puzzleState[line][column+2] == 2 &&
-		!isMoveBanned(move{line, column, "→", puzzleState}) {
-		// right
-		moves = append(moves, move{line, column, "→", puzzleState})
-
-		puzzleState[line][column] = 2
-		puzzleState[line][column+1] = 2
-		puzzleState[line][column+2] = 1
-
-		return true, "→"
+					return true, move{line, column, "→", initialPuzzleState}
+				}
+			}
+		}
 	}
-	return false, "x"
+	return false, move{}
+}
+
+func undo() {
+	bannedMoves = append(bannedMoves, moves[len(moves)-1])
+	puzzleState = moves[len(moves)-1].puzzleState
+	moves = moves[:len(moves)-1]
+
+	pegsOnBoard++
 }
 
 func isMoveBanned(newMove move) bool {
@@ -120,35 +126,22 @@ func isMoveBanned(newMove move) bool {
 var iteration = 0
 
 func resolve() bool {
-	for line := 0; line < 7; line++ {
-		for column := 0; column < 7; column++ {
-			if puzzleState[line][column] == 1 {
-				foundNextMove, direction := findNextMove(line, column)
-				if foundNextMove {
-					iteration++
+	foundNextMove, newMove := findNextMove()
+	if foundNextMove {
+		iteration++
+		moves = append(moves, newMove)
+		fmt.Printf("\rmoving %d:%d:%s, pegs left : %02d, banned moves : %06d, moves : %06d, timer : %s",
+			newMove.column, newMove.line, newMove.direction, pegsOnBoard, len(bannedMoves), len(moves), time.Now().Sub(startTime))
 
-					fmt.Printf("\rmoving %d:%d:%s, pegs left : %02d, banned moves : %06d, moves : %06d, timer : %s",
-						column, line, direction, pegsOnBoard, len(bannedMoves), len(moves), time.Now().Sub(startTime))
-
-					pegsOnBoard--
-					if pegsOnBoard == 1 {
-						return true
-					}
-
-					return resolve()
-				}
-			}
-
-			if len(moves) > 1 && line == 6 && column == 6 {
-				bannedMoves = append(bannedMoves, moves[len(moves)-1])
-				puzzleState = moves[len(moves)-1].puzzleState
-				moves = moves[:len(moves)-1]
-
-				pegsOnBoard++
-
-				return resolve()
-			}
+		pegsOnBoard--
+		if pegsOnBoard == 1 {
+			return true
 		}
+
+		return resolve()
+	} else {
+		undo()
+		return resolve()
 	}
 	return false
 }
